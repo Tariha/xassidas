10cb4d4d0df5d7e306e286b353b7a64427b76f5b
Linzo99 <sallalioune@gmail.com>
2023-08-01 09:08:00 +0000

    update workflow

---
+++ update_db.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update the database when a new 
+# xassida is added
+
+processed=()
+
+# git the list of midified files
+modified_files=$(git diff --name-only)
+
+for xassida in $modified_files
+do
+  if [[ $xassida == xassidas/**/*.txt ]]; then
+    name=$( echo $xassida | cut -f 4 -d '/'  )
+    author=$( echo $xassida | cut -f 3 -d '/'  )
+    echo "[Updating...] $author --> $name"
+    if [[ " ${processed[@]} " =~ " $name " ]]; then
+      echo "Skipping duplicate xassida $name"
+    else
+      ../venv/bin/python parse_xassida.py -x $name
+      ../venv/bin/python parse_translations.py -x $name
+      ../venv/bin/python parse_author.py -a $author
+      cd ../utils/db
+      ../../venv/bin/python insert.py -x $name
+      echo "Processed xassida $name"
+      processed+=("$name")
+    fi
+  fi
+done;
+
